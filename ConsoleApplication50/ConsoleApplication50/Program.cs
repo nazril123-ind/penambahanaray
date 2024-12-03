68ac7a386c895bfd6adf8f4bd2c99dcad3972bd7
nazril123-ind <supriatmanaril@gmail.com>
2024-12-03 14:29:41 +0700

    Add files via upload

---
+++ ConsoleApplication50/ConsoleApplication50/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication50
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 20, 30, 40 };
+            long sum = SumArray(numbers);
+
+            Console.WriteLine(sum);
+            Console.WriteLine("press any key to continue...");
+            Console.ReadKey();
+        }
+        static long SumArray(int[] numbers)
+        {
+            long sum = 0;
+            foreach (int item in numbers)
+            {
+                sum +
+                = item;
+            }
+            return sum;
+        }
+    }
+}
